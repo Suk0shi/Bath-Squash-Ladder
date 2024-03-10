34c837512290f2ce2b6688c4d07e8be20ba5b187
Sukoshi <hugo.cordon100@gmail.com>
2024-03-10 14:57:07 +0000

    initial commit

--- models/user.js
+++ models/user.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    player: { type: String, required: true, maxLength: 100 },
-    position: { type: String, required: true, maxLength: 100 },
+    player: { type: String, required: true },
+    position: { type: String, required: true },
 });
 
 UserSchema.virtual("url").get(function () {
-    return `/catalog/author/${this._id}`;
+    return `/user/${this._id}`;
 });
 
 module.exports = mongoose.model("User", UserSchema);
