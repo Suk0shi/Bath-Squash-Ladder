30b31c733da62ec9eb9ada680620ac1553d6110c
Sukoshi <hugo.cordon100@gmail.com>
2024-03-12 16:42:32 +0000

    Add pug forms and skeleton structure

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('ladder', { users: [{'position':'1', 'name':'Bob', 'surname':'Taylor'}, 
+  {'position':'2', 'name':'Taylor', 'surname':'Bobson'}], loggedInUser: {'position':'2'} });
 });
 
+router.get('/personal', function(req, res, next) {
+  res.render('personal');
+});
+
+router.get('/global', function(req, res, next) {
+  res.render('global');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/signUp', function(req, res, next) {
+  res.render('signUp');
+});
 module.exports = router;
