54961be6dbe545934bf31da9c1e8d9c0cf2976e9
Sukoshi <hugo.cordon100@gmail.com>
2024-04-03 12:36:58 +0100

    Add challenge data to ladder page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
-const { response } = require('../app');
 var router = express.Router();
 
-const challenges_controller = require("../controllers/challengesController")
-const users_controller = require("../controllers/usersController")
+const challenges_controller = require("../controllers/challengesController");
+const users_controller = require("../controllers/usersController");
 
 /* GET home page. */
 router.get('/', challenges_controller.homepage_get);
